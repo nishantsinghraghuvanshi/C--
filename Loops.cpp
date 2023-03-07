b51c9fd760e3de071692018edfe3bf93bc015cda
nishantsinghraghuvanshi <singhnishant0131@gmail.com>
2023-03-07 23:54:41 +0530

    Changes #1

---
+++ Loops.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    if (a == 0);
+    cout << "Zero";
+    bool b = true;
+    while (b = true);
+    cout << "Hello World !!";
+    return 0;
+}
